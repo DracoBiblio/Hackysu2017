ca867c12a7ed28768da5044e792e8333d307e424
Alvingle <austin_vingle@aol.com>
2017-02-18 17:28:22 -0500

    Add files via upload

--- HackYSU.py
+++ HackYSU.py
@@ -18,37 +18,15 @@ class spider():
 	health = 5
 	power = 1
 	defense = 0
-	inventory = ["spider venom"]
+	inventory = []
+	def __str__(self):
+		return "spider"
 
 class orc():
-	health = 20
+	health = 50
 	power = 5
-	defense = 2
+	defense = 1
 	inventory = ["club"]
-
-class goblin():
-	health = 15
-	power = 4
-	defense = 1
-	inventory = ["healing potion"]
-
-class bat():
-	health = 10
-	power = 4
-	defense = 1
-	inventory = ["bat fang"]
-
-class rat():
-	health = 5
-	power = 3
-	defense = 0
-	inventory = ["rat tail"]
-
-class rocktopus():
-	health = 10
-	power = 6
-	defense = 4
-	inventory = ["obsidian shield"]
 
 
 class character ():
@@ -64,22 +42,35 @@ class character ():
 	flag3 = False
 	flag3_1 = False
 	flag4 = False
-	flag5 = False
+	help = ["Your commands are:", '\n' "move north","look","search 'thing'","status","attack 'creature'", "inventory", '\n' "*In battle commands*:","run","attack",'\n' "*Vender commands*:","sell","talk","buy"]
+# def help ():
+# 	commands = ["look",
+# 	"search",
+# 	"status",
+# 	"attack 'creature'",
+# 	"*In battle commands*:"
+# 	"run",
+# 	"buy"
+# 	"*Vender commands*:"
+# 	"sell"
+# 	"talk"]
 
 def reset_enemies():
 	mirror.health = 1
 	mirror.inventory = ['shard of glass']
 	spider.health = 5
 	spider.inventory = []
-	orc.health = 50
-	orc.inventory = ["club"]
 def areaOne(userInput):
 
 	room_inventory = ["shovel"]
 
-	if userInput.lower() == 'n' or 'north' or 'move n' or 'move north':
+	if userInput.lower() == "move north":
 		print("Entering Area Two")
 		return 2
+	elif userInput.lower() == 'help':
+		for element in character.help:
+			print(element)
+		return 1
 	elif userInput.lower() == 'health':
 		print(character.health)
 		return 1
@@ -87,13 +78,13 @@ def areaOne(userInput):
 		print(character.inventory)
 		return 1
 	elif userInput.lower() == 'status':
-		print("You are currently wielding", character.leftarm, character.rightarm, "and you have", character.health, "health", character.power, "power", character.gold, "gold")
+		print("You are currently wielding", character.leftarm, character.rightarm, "and you have", character.health, "health", character.power, "power", character.defense, "defense", character.gold, "gold")
 		return 1
 	# elif userInput.lower() == 'w':
 	# 	return 2
 	elif character.flag1 == False and userInput.lower() == 'search chest':
 		print("You found", room_inventory)
-		character.rightarm += ['shovel']
+		character.rightarm = ['shovel']
 		character.power += 2
 		character.flag1 = True
 		return 1
@@ -153,7 +144,7 @@ def combat_areaOne_mirror (userInput):
 
 def areaTwo(userInput):
 
-	if userInput.lower() == 'n':
+	if userInput.lower() == "move north":
 		print("Entering Area Three")
 		return 3
 	elif userInput.lower() == 'search':
@@ -181,7 +172,7 @@ def combat_areaTwo_spider (userInput):
 	incombat = True
 	while(incombat == True):
 		if userInput.lower() == 'attack':
-			print('You swing at', spider, 'with your', character.rightarm, 'dealing', character.power - spider.defense, 'damage.')
+			print('You swing at', 'spider', 'with your', character.rightarm, 'dealing', character.power - spider.defense, 'damage.')
 			spider.health -= (character.power - spider.defense)
 			
 			if(spider.health <= 0):
@@ -195,7 +186,7 @@ def combat_areaTwo_spider (userInput):
 				incombat = False
 				return character.previous_state
 			elif(spider.health > 0):
-				print(spider, 'Attacks you dealing', spider.power - character.defense, 'damage.')
+				print('spider', 'Attacks you dealing', spider.power - character.defense, 'damage.')
 				character.health -= (spider.power - character.defense)
 				return 100
 			elif(character.health <= 0):
@@ -212,7 +203,7 @@ def combat_areaTwo_spider (userInput):
 
 def areaThree(userInput):
 
-	if userInput.lower() == 'n':
+	if userInput.lower() == "move north":
 		print("Entering Area Four. You see a Shop(shop), and a Blacksmith(blacksmith).")
 		return 4
 	elif character.rightarm == ['shovel'] and userInput.lower() == 'use shovel':
@@ -243,7 +234,7 @@ def areaThree(userInput):
 	pass
 
 def areaFour(userInput):
-	if userInput.lower() == 'n':
+	if userInput.lower() == "move north":
 		if character.flag4 == False:
 			print("You are ambushed by an orc!")
 			print("The orc lands a pre-emptive strike for", orc.power - character.defense, "damage.")
@@ -268,47 +259,73 @@ def areaFour(userInput):
 	 	print("invalid input")
 	 	return 4
 	pass
-def shop(userInput):		
+
+def shop(userInput):
 	if userInput.lower() == 'exit':
-		print("Entering Area Four")		
-		return 4		
+		print("Entering Area Four. You see a Shop(shop), and a Blacksmith(blacksmith).")
+		return 4
 	elif character.rightarm == ['shovel'] and userInput.lower() == 'talk':
-		print("You ask the Shop about her wares.")		
-		print("'I only have 5 healing potions, but I am willing to buy your shovel and shield for 15 gold'")		
-		return 4.1		
-	elif userInput.lower() == 'talk':		
-		print("You ask the Shop keep about her wares")		
-		print("'I can offer 5 healing potions for 5 gold each.'")		
-		return 4.1		
-	elif character.gold == 0 and userInput.lower() == 'buy':		
-		print('You do not have enough gold.')		
-		return 4.1		
-	elif character.rightarm == ['shovel'] and userInput.lower() == 'sell':		
-		print("You sell your shovel for 15 gold")		
-		character.rightarm = []		
-		character.power = 2		
-		character.gold += 15		
-		return 4.1		
-	elif userInput.lower() == 'buy':		
-		print("You purchase a healing potion for 5 gold.")		
-		character.gold -= 5		
-		character.inventory += ['healing potion']		
-		return 4.1		
-	elif userInput.lower() == 'status':		
-		print("You are currently wielding", character.leftarm, character.rightarm, "and you have", character.health, "health", character.power, "power", character.gold, "gold")		
-		return 4.1		
+		print("You ask the Shop about her wares.")
+		print("'I only have 5 healing potions, but I am willing to buy your shovel for 15 gold'")
+		return 4.1
+	elif userInput.lower() == 'talk':
+		print("You ask the Shop keep about her wares")
+		print("'I can offer healing potions for 5 gold each.'")
+		return 4.1
+	elif character.gold == 0 and userInput.lower() == 'buy':
+		print('You do not have enough gold.')
+		return 4.1
+	elif character.rightarm == ['shovel'] and userInput.lower() == 'sell':
+		print("You sell your shovel for 15 gold")
+		character.rightarm = ['fists']
+		character.power = 2
+		character.gold += 15
+		return 4.1
+	elif userInput.lower() == 'buy':
+		print("You purchase a healing potion for 5 gold.")
+		character.gold -= 5
+		character.inventory += ['healing potion']
+		return 4.1
+	elif userInput.lower() == 'status':
+		print("You are currently wielding", character.leftarm, character.rightarm, "and you have", character.health, "health", character.power, "power", character.gold, "gold")
+		return 4.1
+	elif userInput.lower() == 'inventory':
+		print("inventory", character.inventory)
+		return 4.1
+	else:
+		print("invalid input")
+		return 4.1
 	
-def blacksmith(userInput):		
- 	if userInput.lower() == 'exit':		
- 		print("Entering Area Four")		
- 		return 4		
- 	elif userInput.lower() == 'talk':		
- 		print("You ask the Blacksmith about his items.")		
- 		print("I have a trusty sword for sale for 15 gold.")		
- 		return 4.2
+	
+def blacksmith(userInput):
+	if userInput.lower() == 'exit':
+		print("Entering Area Four. You see a Shop(shop), and a Blacksmith(blacksmith).")
+		return 4
+	elif userInput.lower() == 'talk':
+		print("You ask the Blacksmith about his items.")
+		print("'I have a trusty sword for 14 gold.'")
+		return 4.2
+	elif character.gold >= 15 and userInput.lower() == 'buy':
+		print('You purchased the sword')
+		character.gold -= 14
+		character.rightarm = ['sword']
+		character.power = 10
+		return 4.2
+	elif character.gold < 15 and userInput.lower() == 'buy':
+		print('You do not have enough coins')
+		return 4.2
+	elif userInput.lower() == 'status':
+		print("You are currently wielding", character.leftarm, character.rightarm, "and you have", character.health, "health", character.power, "power", character.gold, "gold")
+		return 4.2
+	else:
+		print("Invalid input")
+		return 4.2
+
 def areaFive(userInput):
-	if userInput.lower() == 'n':
-		print("Entering Area Six")
+	if userInput.lower() == "move north":
+		print("Entering Area Six.")
+		print("A mist slowly fills around you, covering everything you once saw")
+		print("A voice echos, You must go into the dongeon to find the eight peices of the trinity to activate the portal in Young's town")
 		return 6
 	elif userInput.lower() == 'search':
 		print('You did not find anything')
@@ -325,7 +342,7 @@ def combat_areaFive_orc (userInput):
 	while(incombat == True):
 		character.flag4 = True
 		if userInput.lower() == 'attack':
-			print('You swing at', orc, 'with your', character.rightarm, 'dealing', character.power - orc.defense, 'damage.')
+			print('You swing at', 'orc', 'with your', character.rightarm, 'dealing', character.power - orc.defense, 'damage.')
 			orc.health -= (character.power - orc.defense)
 			
 			if(orc.health <= 0):
@@ -339,7 +356,7 @@ def combat_areaFive_orc (userInput):
 				incombat = False
 				return character.previous_state
 			elif(orc.health > 0):
-				print(orc, 'Attacks you dealing', orc.power - character.defense, 'damage.')
+				print('orc', 'Attacks you dealing', orc.power - character.defense, 'damage.')
 				character.health -= (orc.power - character.defense)
 				return 102
 			elif(character.health <= 0):
@@ -355,7 +372,7 @@ def combat_areaFive_orc (userInput):
 		else:
 			return 102
 def areaSix(userInput):
-	if userInput.lower() == 'n':
+	if userInput.lower() == "move north":
 		print("Entering Area Seven")
 		return 7
 	elif userInput.lower() == 'search':
@@ -370,87 +387,22 @@ def areaSix(userInput):
 	pass
 
 def areaSeven(userInput):
-	if userInput.lower() == 'n':
+	if userInput.lower() == "move north":
 		print("Entering Area Eight")
 		return 8
 	elif userInput.lower() == 'search':
 		print('You did not find anything')
 		return 7
-	elif character.flag5 == False and userInput.lower() == 'look':
-		print('You see the entrance to a cave being "guarded" by a sleeping goblin')
+	elif userInput.lower() == 'look':
+		print('You did not see anything')
 		return 7
-	elif character.flag5 == True and userInput.lower() == 'look':
-		print('You see the entrance to the cave being guarded by a dead goblin')
-	elif character.flag5 == False and userInput.lower() == 'enter cave':
-		print("The sound of your approach wakes the goblin!")
-		print("The goblin springs to action, landing the first strike on you for", goblin.power - character.defense, "damage!")
-		character.health -= goblin.power - character.defense
-		print('You are in combat with goblin')
-		character.previous_state = 7
-		return 103
-	elif character.flag5 == True and userInput.lower() == 'enter cave':
-		print("Entering Dungeon")
-		return 90	
-	elif userInput.lower() == 'attack goblin':
-		print('You are in combat with goblin')
-		character.previous_state = 7
-		return 103
 	else:
 	 	print("invalid input")
 	 	return 7
 	pass
 
-def combat_areaSeven_goblin(userInput):
-	incombat = True
-	while(incombat == True):
-		if userInput.lower() == 'attack':
-			print('You swing at the goblin with your', character.rightarm, 'dealing', character.power - goblin.defense, 'damage.')
-			goblin.health -= (character.power - goblin.defense)
-			
-			if(goblin.health <= 0):
-				print('You have defeated goblin')
-				character.flag5 = True
-				print('You find', goblin.inventory, 'on the body.')
-				character.inventory += goblin.inventory
-				print("Entering Dungeon")	
-				reset_enemies()
-				incombat = False
-				return character.previous_state
-			elif(goblin.health > 0):
-				print('The goblin attacks you dealing', goblin.power - character.defense, 'damage.')
-				character.health -= (goblin.power - character.defense)
-				return 103
-			elif(character.health <= 0):
-				print('You have died')
-				print('Game Over')
-			else:
-				return 103
-		elif userInput.lower() == 'run':
-			print('You manage to escape.')
-			print("Returning to area 7")	
-			incombat = False
-			return character.previous_state
-		else:
-			return 103
-
-def dungeoneOne_roomOne(userInput):
-	if userInput.lower() == 'n':
-		print("Entering Dungeon Room 2")
-		return 91
-	elif userInput.lower() == 's' or 'south' or 'exit' or 'leave' or 'move south':
-		return 7
-	elif userInput.lower() == 'search':
-		print('You did not find anything')
-		return 90
-	elif userInput.lower() == 'look':
-		print('You did not see anything')
-		return 90
-	else:
-		 print("invalid input")
-		 return 90
-	pass
 def areaEight(userInput):
-	if userInput.lower() == 'n':
+	if userInput.lower() == "move north":
 		print("Entering Area Nine")
 		return 9
 	elif userInput.lower() == 'search':
@@ -465,7 +417,7 @@ def areaEight(userInput):
 	pass
 
 def areaNine(userInput):
-	if userInput.lower() == 'n':
+	if userInput.lower() == "move north":
 		return 9
 	elif userInput.lower() == 'search':
 		print('You did not find anything')
@@ -489,11 +441,9 @@ dictionaryOfAreas = {
 	7: areaSeven,
 	8: areaEight,
 	9: areaNine,
-	90: dungeoneOne_roomOne,
 	100: combat_areaTwo_spider,
 	101: combat_areaOne_mirror,
-	102: combat_areaFive_orc,
-	103: combat_areaSeven_goblin
+	102: combat_areaFive_orc
 }
 
 def gameLoop():
